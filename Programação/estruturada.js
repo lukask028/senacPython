c34f5e5c726946ec40327743a185dc33062fcb04
lukask028 <54475600+lukask028@users.noreply.github.com>
2024-03-20 21:44:33 -0300

    Add files via upload

---
+++ Programação/estruturada.js
@@ -0,0 +1,12 @@
+//programacao estruturada
+
+idade = 20;
+if (idade >= 18){
+
+    // estrutura de decisão, para verificar a idade
+    
+    console.log("É maior de idade, pode entrar na festa!")
+}
+else {
+   console.log("É menor de idade, não pode entrar na festa!")
+}
---
+++ Programação/orientada.js
@@ -0,0 +1,15 @@
+//programacao orientada a objeto (poo)
+//molde
+class VerificaIdade{
+    //atributos sao variaveis das classes, caracteristicas
+    idade
+
+    //metodo, é uma ação do código. 
+    verificar(){
+        if (this.idadeidade >= 18){
+            console.log("Você é maior de idade, pode entrar!!")
+        } else {
+            console.log("Você é menor de idade, não pode entrar!!")
+        }
+    }
+}
